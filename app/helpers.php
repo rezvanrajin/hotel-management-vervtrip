bc53425a8a42fdb0997f907bea39cf0186b5c602
rezvanrajin <razin.rejvan@gmail.com>
2025-09-14 22:08:16 +0600

    simple changes files

--- app/helpers.php
+++ app/helpers.php
@@ -73,62 +73,7 @@ class Helper
 		});
 	}
 
-    public static function sendPushNotification($device_token, $title, $body, $image)
-    {
-        $data = [
-            'to' => $device_token,
-            'content_available' => true,
-            "sticky" => true,
-            'notification' => [
-                'title' => $title,
-                'body' => $body,
-                'style' => 'picture',
-                "image" => env("APP_URL") . $image,
-            ],
-            'data' => [
-                'title' => $title,
-                'body' => json_encode($body),
-                'status' => 'done',
-                "image" => env("APP_URL") . $image,
-            ],
-        ];
-        $curl = curl_init();
-        curl_setopt_array(
-            $curl,
-            array(
-                CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "POST",
-                CURLOPT_POSTFIELDS => json_encode($data),
-                CURLOPT_HTTPHEADER => array(
-                    "Authorization: key=AAAAIZKFubk:APA91bEy2Vc3H0-f6OO6ZggCJ8fuz8PRZjIlbYQgZc2HY7ctVdgJ88zOtfTHW1GjxHVCajpztOG-dPQOWmq4rvambuX8nXTHCgs7v9xU9KjR9Jdf3TBSt4V1SkU1yg73rPoVx66fGGJ7",
-                    "Content-Type: application/json"
-                ),
-            )
-        );
-        $response = curl_exec($curl);
-        curl_close($curl);
-        return json_decode($response);
-    }
-
-    public static function storeNotification($user_id, $order_id, $notification_title, $notification_description, $receiver_id = null, $click_action = null, $status = null , $receiver_role = null)
-    {
-        $notification = new Notification();
-        $notification->user_id = $user_id;
-        $notification->order_id = $order_id;
-        $notification->title = $notification_title;
-        $notification->description = $notification_description;
-        $notification->receiver_id = $receiver_id;
-        $notification->click_action = $click_action;
-        $notification->status = $status;
-        $notification->receiver_role = $receiver_role ? $receiver_role : null;
-        $notification->save();
-    }
+  
 
 
 }
